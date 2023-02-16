e2d3a6b50497e2c1960349ff459d9e55fc8daf27
KROCCO <kylerocco08@gmail.com>
2023-02-16 07:33:13 -0500

    2/15/23

--- SpheroBolt.js
+++ SpheroBolt.js
@@ -1,8 +1,8 @@
 /*
 Programming Team: Ranen Allishaw & Kyle Rocco
-Date: FEB.13.2023
+Date: FEB.15.2023
 Project: Labyrinth Competition
-Version: 3.3
+Version: 5.2
 Bolt: 183F
 */
 
@@ -25,7 +25,7 @@ async function CheckpointBLUE() {
  *
 */
 async function CheckpointNoise_1(){
-  await roll(90, 70, 1.5);
+  await roll(90, 69.5, 1.5);
   await delay(2);
   await roll(180, 65, 1);
   await spin(42.5, 2.5);
@@ -33,16 +33,63 @@ async function CheckpointNoise_1(){
 }
 
 
+/**
+ *
+*/
 async function CheckpointRED() {
-    await roll(217.5, 53.75, 1.5);
+    await roll(217.5, 56.75, 1.5);
     await delay(2);
-    await roll(132.5, 39.1, 1.1);
+    await roll(132.5, 41, 1.1);
     setMainLed({ r: 255, g: 0, b: 0})
+    await delay(2);
 }
 
 
+/**
+ *
+*/
+async function CheckpointNoise_2() {
+    await roll(42.5, 55.75, 1.5);
+    await delay(1.5);
+    await roll(130, 40.5, 1.75);
+    await Sound.Animal.Monkey.play();
+}
+
+
+/**
+ *
+*/
+async function CheckpointGREEN() {
+    await delay(1.5);
+    await roll(180, 43.75, 1);
+    await delay(1.5);
+    await roll(89.5, 41, 1.1);
+    setMainLed({ r: 0, g: 255, b: 0})
+}
+
+
+/**
+ *
+*/
+async function CheckpointNoise_3() {
+    await roll(89.5, 41, 1.1);
+    await delay(2);
+    await roll(0, 45, 1.5);
+    await delay(2);
+    await roll(315, 45, 1.25);
+    await Sound.Animal.Monkey.play();
+    await delay(2);
+}
+
+
+/**
+ *
+*/
 async function startProgram() {
     await CheckpointBLUE(); // Calls CheckpointBLUE
     await CheckpointNoise_1();
     await CheckpointRED();
+    await CheckpointNoise_2();
+    await CheckpointGREEN();
+    await CheckpointNoise_3();
 }
