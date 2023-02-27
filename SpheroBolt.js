fc11b70d7d89d87eb59cf306348468e33130458e
KROCCO <kylerocco08@gmail.com>
2023-02-27 07:47:38 -0500

    Red

--- SpheroBolt.js
+++ SpheroBolt.js
@@ -31,4 +31,14 @@ async function CheckpointNoise_1(){
   await roll(180, 66.25, 1);
   await spin(42.5, 2.5);
   await Sound.Animal.play();
-}
+
+
+/**
+ *
+*/
+async function CheckpointRED() {
+    await roll(217.5, 56.1, 1.5);
+    await delay(2);
+    await roll(132.5, 40.75, 1.1);
+    setMainLed({ r: 255, g: 0, b: 0})
+    await delay(2);
