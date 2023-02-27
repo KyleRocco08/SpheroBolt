72f402d13e4aa324105ffa77df10c1fd1da11da3
KROCCO <kylerocco08@gmail.com>
2023-02-27 07:45:55 -0500

    Blue

--- SpheroBolt.js
+++ SpheroBolt.js
@@ -1,3 +1,23 @@
 //Programmer: Kyle Rocco
-//Date: 2.6.23
-//Project: Sphero Bolt
+//Date:/*
+Programming Team: Ranen Allishaw & Kyle Rocco
+Date: FEB.15.2023
+Project: Labyrinth Competition
+Version: 7.2
+Bolt: 183F
+*/
+
+
+/**
+ * CheckpointBLUE will have START scroll across the LED
+ * Then it will roll at 0 degrees at 30 speed for 2 seconds
+ * Rotate 90 CW
+ * Finally it will have the LED turn BLUE
+*/
+async function CheckpointBLUE() {
+  await scrollMatrixText('START', { r: 255, g: 255, b: 255 }, 30, true);
+  await roll(0, 78, 2);
+  await spin(90, 2.5);
+  setMainLed({ r: 0, g: 0, b: 255 })
+}
+
